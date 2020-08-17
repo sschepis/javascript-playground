473282b3cc4079636387390829e7017c0a2546fe
Sebastian Schepis <sschepis@gmail.com>
2020-08-16 21:38:58 -0700

    updates and cleanups

--- src/components/title-bar/index.js
+++ src/components/title-bar/index.js
@@ -48,14 +48,15 @@ export default class TitleBar extends Component {
 
   render () {
     return (
-      <div style={classes} className={'titlebar grabbing'}><div className='grid-stack-item-content'>
+      <div style={classes} className={'titlebar'}>
+          <i style={{ marginLeft: 2, marginTop: 2, marginRight: 5 }} className='fa fa-dot-circle-o grid-stack-item-content' />
           <i style={{ marginLeft: 2, marginTop: 2 }} className='fa fa-caret-down' />
           <i style={{ marginLeft: 2, marginTop: 2 }} className='fa fa-caret-up' />
-        <span className={'title-text'}>{this.props.text}</span>
-          <i style={{ marginRight: 2, marginTop: 2, float: 'right' }} className='fa fa-times' />
-          <i style={{ marginRight: 2, marginTop: 2, float: 'right' }} className='fa fa-arrow-down' />
-          <i style={{ marginRight: 2, marginTop: 2, float: 'right' }} className='fa fa-arrow-up' />
-      </div></div>
+          <span className={'title-text'}>{this.props.text}</span>
+          <i style={{ marginRight: 4, marginTop: 4, float: 'right' }} className='fa fa-times' />
+          <i style={{ marginRight: 4, marginTop: 4, float: 'right' }} className='fa fa-arrow-down' />
+          <button style={{ float: 'right' }}  onClick={this.handleArrowUp}><i style={{ marginRight: 4, marginTop: 4 }}  className='fa fa-arrow-up' /></button>
+      </div>
     )
   }
 }
